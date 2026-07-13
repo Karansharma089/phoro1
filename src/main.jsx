23542249dde08f73476a9ced34d0be12a535a856
Karan Sharma <karan007sharma009@gmail.com>
2026-07-14 01:33:23 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -15,12 +15,12 @@ import ReactDOM from "react-dom/client";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import App from "./app.jsx";
 import "./theme.css";
-
+ 
 const GOOGLE_CLIENT_ID = "814801522934-8dvck6vll18lghp0oaii6ah32buqtepv.apps.googleusercontent.com";
-
+ 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId={814801522934-8dvck6vll18lghp0oaii6ah32buqtepv.apps.googleusercontent.com}>
+    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
       <App />
     </GoogleOAuthProvider>
   </React.StrictMode>
